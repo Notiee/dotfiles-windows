66e101214f3b8b9bb4652738f08caca4691cfd4e
Nota <scope.dk@gmail.com>
2023-04-21 23:18:08 +0200

    Testing whitespace settings

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -12,23 +12,27 @@ return {
       }
     },
   },
-
   -- Notifications (message boxes) for nvim, TODO: Configure to actually give notifications
   { "rcarriga/nvim-notify", opts = {}},
-
   -- Shows colors behind RGB and HEX values etc..
   { "NvChad/nvim-colorizer.lua", opts = {}},
-
   -- Indent guides
   {
     "lukas-reineke/indent-blankline.nvim",
     config = function ()
+      --vim.opt.list = true
+      --local listchars = vim.opt.listchars
+      --listchars:append("trail:•")
+      --listchars:append("lead:•")
+      --listchars:append("space:•")
+      --vim.g.indent_blankline_char = ">"
       require("indent_blankline").setup {
+        --char = "▏",
+        --context_char = "▏",
+        no_tab_character = true,
         show_current_context = true,
-        -- show_current_context_start = true,
         show_end_of_line = true,
-        show_trailing_blankline_indent = false,
-        space_char_blankline = " ",
+        show_trailing_blankline_indent = true,
         use_treesitter = true,
       }
     end
