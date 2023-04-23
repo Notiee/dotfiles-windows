8a8835b6dbaaf1ce6d446fb7ae695da8c47c3809
Nota <scope.dk@gmail.com>
2023-04-23 19:54:53 +0200

    Update ui.lua
    Added initial customization for alpha.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,24 @@
 -- Contains UI plugins in one file
 return {
+  -- Notifications (message boxes) for nvim, TODO: Configure to actually give notifications
+  { "rcarriga/nvim-notify", opts = {}},
+  -- Shows colors behind RGB and HEX values etc..
+  { "NvChad/nvim-colorizer.lua", opts = {}},
+  -- Indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function ()
+      require("indent_blankline").setup {
+        --char = "▏",
+        --context_char = "▏",
+        no_tab_character = true,
+        show_current_context = true,
+        show_end_of_line = true,
+        --show_trailing_blankline_indent = true,
+        use_treesitter = true,
+      }
+    end
+  },
   -- Lualine, replace default status line with a customizable one written in Lua
   {
     'nvim-lualine/lualine.nvim',
@@ -12,37 +31,28 @@ return {
       }
     },
   },
-  -- Notifications (message boxes) for nvim, TODO: Configure to actually give notifications
-  { "rcarriga/nvim-notify", opts = {}},
-  -- Shows colors behind RGB and HEX values etc..
-  { "NvChad/nvim-colorizer.lua", opts = {}},
-  -- Indent guides
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function ()
-      --vim.opt.list = true
-      --local listchars = vim.opt.listchars
-      --listchars:append("trail:•")
-      --listchars:append("lead:•")
-      --listchars:append("space:•")
-      --vim.g.indent_blankline_char = ">"
-      require("indent_blankline").setup {
-        --char = "▏",
-        --context_char = "▏",
-        no_tab_character = true,
-        show_current_context = true,
-        show_end_of_line = true,
-        show_trailing_blankline_indent = true,
-        use_treesitter = true,
-      }
-    end
-  },
   -- Greeter/Dashboard screen
   {
     'goolord/alpha-nvim',
-    -- requires = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    event = 'VimEnter',
+    -- We modify theme (dashboard) to our liking aswell as making buttons adhere to my nvim config
+    opts = function()
+      local dashboard = require('alpha.themes.dashboard')
+      local logo = [[
+      +------+.      +------+       +------+       +------+      .+------+
+      |`.    | `.    |\     |\      |      |      /|     /|    .' |    .'|
+      |  `+--+---+   | +----+-+     +------+     +-+----+ |   +---+--+'  |
+      |   |  |   |   | |    | |     |      |     | |    | |   |   |  |   |
+      +---+--+.  |   +-+----+ |     +------+     | +----+-+   |  .+--+---+
+      `.  |    `.|    \|     \|     |      |     |/     |/    |.'    | .'
+        ` +------+     +------+     +------+     +------+     +------+'     
+      ]]
+      dashboard.section.header.val = vim.split(logo, '\n')
+      return dashboard
+    end,
+    -- Initialize alpha with the theme of choice (dashboard)
+    config = function()
+      require('alpha').setup(require('alpha.themes.dashboard').config)
     end
   }
 }
